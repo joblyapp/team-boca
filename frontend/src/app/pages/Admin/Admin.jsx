ab17dffefdc96b2ac455cd4703d9ccec0590dd8a
Frankcisco99 <franciscofigueroabecerra@gmail.com>
2023-01-26 19:07:44 -0300

    Finalizacion panel de administrador

--- frontend/src/app/pages/Admin/Admin.jsx
+++ frontend/src/app/pages/Admin/Admin.jsx
@@ -1,28 +1,29 @@
 import {useState} from 'react'
 import { useParams } from 'react-router-dom';
+import Menu from '../../components/Menu/Menu';
 import Post from '../../components/Post/Post';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Suggestion from '../../components/Suggestion/Suggestion';
+import Tabs from '../../components/Tabs/Tabs';
 import "./Admin.css";
 const Admin = () => {
     const [tab, setTab] = useState(true);
   return (
     <div className="admin">
-        <Sidebar/>
+        <div className="admin-sidebar">
+            <Sidebar/>
+        </div>
         <div className="admin-container">
             <div className="admin-feed">
-                <div className="admin-title">
-                    <h1>Panel de Administración</h1>
-                </div>
+                <Menu nombre="Panel administrador"/>
+                <Tabs tab1="Mis posts" tab2="Posts de usuarios"/>
                 <div className="admin-content">
-                    
-
                     <Post/>
                     <Post/>
                     <Post/>
                 </div>
             </div>
-            <div>
+            <div className="admin-suggestion">
                 <Suggestion/>
             </div>
         </div>
